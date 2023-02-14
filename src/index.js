d11279174fe2e2836dc9129195799d33abefbae2
youngchae <010101chae@gmail.com>
2023-02-14 16:24:53 +0900

    third commit

--- src/index.js
+++ src/index.js
@@ -1,88 +1,86 @@
 const wrap = document.querySelector('.wrap');
 const section = document.querySelector('#section');
-
-const preBtn = document.createElement('div');
-preBtn.innerHTML = '<i class="fa-solid fa-circle-chevron-left"></i>';
-section.appendChild(preBtn);
-
-const carousel = document.createElement('div');
-carousel.classList.add('carousel-wrap');
-section.appendChild(carousel);
+const previousButton = document.createElement('div');
+const nextButton = document.createElement('div');
+const carouselWrap = document.createElement('div');
 const image = document.createElement('img');
-carousel.appendChild(image);
+const dotButtons = document.createElement('div');
+
+// left 버튼 추가
+previousButton.innerHTML = '<i class="fa-solid fa-circle-chevron-left"></i>';
+section.appendChild(previousButton);
+
+// 이미지 추가
+carouselWrap.classList.add('carousel-wrap');
+section.appendChild(carouselWrap);
+carouselWrap.appendChild(image);
 image.src = '/src/1.png';
 image.width = '400';
 image.height = '400';
 
-const nextBtn = document.createElement('div');
-nextBtn.innerHTML = '<i class="fa-solid fa-circle-chevron-right"></i>';
-section.appendChild(nextBtn);
+// right 버튼 추가
+nextButton.innerHTML = '<i class="fa-solid fa-circle-chevron-right"></i>';
+section.appendChild(nextButton);
 
-const buttons = document.createElement('div');
-wrap.appendChild(buttons);
-buttons.classList.add('dotBtns');
-
+// 도트 버튼 5개 추가
+wrap.appendChild(dotButtons);
+dotButtons.classList.add('dotButtons');
 for (let i = 1; i <= 5; i++) {
-  const listBtn = document.createElement('li');
-  listBtn.id = 'list' + i.toString();
-  listBtn.classList.add('dotBtnList');
-  buttons.appendChild(listBtn);
-  listBtn.innerHTML = '●';
-  listBtn.style.cursor = 'default';
+  const listButton = document.createElement('li');
+  listButton.id = 'list' + i.toString();
+  dotButtons.appendChild(listButton);
+  listButton.innerHTML = '●';
+  listButton.style.cursor = 'default';
 }
 
+// 도트 5개 변수 설정
 const list1 = document.querySelector('#list1');
 const list2 = document.querySelector('#list2');
 const list3 = document.querySelector('#list3');
 const list4 = document.querySelector('#list4');
 const list5 = document.querySelector('#list5');
 
-const carouselWrap = document.querySelector('.carousel-wrap');
-
+// 초기화면
 list1.style.color = 'tomato';
+let presentNumber = 1;
 
-list1.addEventListener('click', () => {changeImg(1)});
-list2.addEventListener('click', () => {changeImg(2)});
-list3.addEventListener('click', () => {changeImg(3)});
-list4.addEventListener('click', () => {changeImg(4)});
-list5.addEventListener('click', () => {changeImg(5)});
-
-function changeImg(picNum) {
-  image.src = `/src/${picNum}.png`;
-  if (picNum === 4) image.height = '300';
-  else if (picNum === 5) image.height = '220';
+// 이미지 및 도트 컬러 변경 함수
+function changeImgAndDotColor(pictureNumber) {
+  image.src = `/src/${pictureNumber}.png`;
+  if (pictureNumber === 4) image.height = '300';
+  else if (pictureNumber === 5) image.height = '220';
   else image.height = '400';
-  const allLi = document.querySelectorAll('li');
-  for (let i = 0; i < allLi.length; i++) {
-    allLi[i].style.color = 'black';
+  const dots = document.querySelectorAll('li');
+  for (let i = 0; i <= 4; i++) {
+    dots[i].style.color = 'black';
+    if (i === pictureNumber - 1) {
+      dots[i].style.color = 'tomato';
+    }
   }
-  const li = document.querySelector(`#list${picNum}`);
-  li.style.cssText = 'list-style: none; color: tomato; cursor: default';
+  presentNumber = pictureNumber;
 }
 
-let presentNum = 1;
+// 도트버튼 클릭 이벤트 추가
+list1.addEventListener('click', () => changeImgAndDotColor(1));
+list2.addEventListener('click', () => changeImgAndDotColor(2));
+list3.addEventListener('click', () => changeImgAndDotColor(3));
+list4.addEventListener('click', () => changeImgAndDotColor(4));
+list5.addEventListener('click', () => changeImgAndDotColor(5));
 
-nextBtn.addEventListener('click', () => {goNext(presentNum + 1)});
-preBtn.addEventListener('click', () => {goPre(presentNum - 1)})
-
-function goNext(picNum) {
-  if (picNum <= 0) picNum += 5;
-  else if (picNum > 5) picNum %= 5;
-  console.log(picNum);
-  changeImg(picNum);
-  presentNum += 1;
-  if (presentNum <= 0) presentNum += 5;
-  else if (presentNum > 5) presentNum %= 5;
-  console.log(presentNum);
+// 다음 사진 요청 함수
+function goNextPicture(pictureNumber) {
+  if (pictureNumber <= 0) pictureNumber += 5;
+  else if (pictureNumber > 5) pictureNumber %= 5;
+  changeImgAndDotColor(pictureNumber);
 }
 
-function goPre(picNum) {
-  if (picNum <= 0) picNum += 5;
-  else if (picNum > 5) picNum %= 5;
-  console.log(picNum);
-  changeImg(picNum);
-  presentNum -= 1;
-  if (presentNum <= 0) presentNum += 5;
-  else if (presentNum > 5) presentNum %= 5;
-  console.log(presentNum);
+// 이전 사진 요청 함수
+function goPreviousPicture(pictureNumber) {
+  if (pictureNumber <= 0) pictureNumber += 5;
+  else if (pictureNumber > 5) pictureNumber %= 5;
+  changeImgAndDotColor(pictureNumber);
 }
+
+// 다음, 이전 버튼 이벤트 추가
+nextButton.addEventListener('click', () => goNextPicture(presentNumber + 1));
+previousButton.addEventListener('click', () => goPreviousPicture(presentNumber - 1));
